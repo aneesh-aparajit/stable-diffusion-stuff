c3bff771fbaacf1bdd996c731b3dd162a5aa0cf8
aneesh-aparajit <aneeshaparajit.g2002@gmail.com>
2023-06-07 00:11:45 +0530

    implemented img2img

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 ## Text to Image
 
-|PROMPT|OUTPUT|
-|------|------|
-|A watercolor painting of an otter|![otter](./assets/text2img.png)|
-|A picture of an anime character|![anime](./assets/text2img_anime.png)
+| PROMPT                                    | OUTPUT                                   |
+|-------------------------------------------|------------------------------------------|
+| A picture of Naruto.                      | ![naruto](./assets/text2img_naruto.png)  |
+| A oil painting of an anime character      | ![anime](./assets/anime_text2img.png)    |
+| A picture of 2 anime characters fighting  | ![person](./assets/text2image_fight.png) |
+
+
+## Image to Image
+| INPUT IMAGE                  | OUTPUT IMAGE                    |
+|------------------------------|---------------------------------|
+| ![input](./assets/macaw.jpg) | ![output](./assets/img2img.png) |
--- src/img2img.py
+++ src/img2img.py
@@ -5,6 +5,7 @@ from PIL import Image
 from torch import autocast
 from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer, logging
+from torchvision import transforms as tfms
 
 torch.manual_seed(1)
 
@@ -14,10 +15,10 @@ logging.set_verbosity_error()
 # Set device
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Load the autoencoder model which will be used to decode the latents into image space. 
+# Load the autoencoder model which will be used to decode the latents into image space.
 vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
 
-# Load the tokenizer and text encoder to tokenize and encode the text. 
+# Load the tokenizer and text encoder to tokenize and encode the text.
 tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
 text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
 
@@ -43,10 +44,35 @@ width  = 512
 batch_size = 1
 guidance_scale = 8
 
-# -------------------------- Set Inference Timesteps ------------------------- #
-scheduler.set_timesteps(num_inference_steps=num_inference_timesteps)
+input_image = Image.open('../assets/macaw.jpg').resize((512, 512))
 
-latents = torch.randn((batch_size, unet.config.in_channels, height//8, width//8), generator=generator)
+def pil_to_latent(input_im):
+    # Single image -> single latent in a batch (so size 1, 4, 64, 64)
+    with torch.no_grad():
+        latent = vae.encode(tfms.ToTensor()(input_im).unsqueeze(0).to(torch_device)*2-1) # Note scaling
+    return 0.18215 * latent.latent_dist.sample()
+
+def latents_to_pil(latents):
+    # bath of latents -> list of images
+    latents = (1 / 0.18215) * latents
+    with torch.no_grad():
+        image = vae.decode(latents).sample
+    image = (image / 2 + 0.5).clamp(0, 1)
+    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+    images = (image * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
+    return pil_images
+
+
+encoded = pil_to_latent(input_image)
+'''
+We want to convert one image to another. So, what we can do is, use the prompt for guidance, and we can also start from src
+image itself. So, what we do to do the denoising process is use the VAE output of the source image and then add noise to the latents.
+Hopefully, the final latents we land on, when upscaled can give us what we want.
+'''
+scheduler.set_timesteps(num_inference_steps=num_inference_timesteps)
+noise = torch.randn_like(encoded, generator=generator)
+latents = scheduler.add_noise(encoded, noise, 0)
 latents = latents * scheduler.init_noise_sigma
 
 prompt_embeds = tokenizer(prompt, return_tensors="pt", max_length=tokenizer.model_max_length, truncation=True, padding="max_length")
@@ -55,28 +81,19 @@ uncond_embeds = tokenizer([""], return_tensors="pt", max_length=prompt_embeds.in
 with torch.no_grad():
     prompt_embeds = text_encoder(prompt_embeds.input_ids.to(torch_device)).last_hidden_state
     uncond_embeds = text_encoder(uncond_embeds.input_ids.to(torch_device)).last_hidden_state
-    embeddings = torch.cat([uncond_embeds, prompt_embeds]).to(torch_device)
+    embeddings = torch.cat([uncond_embeds, prompt_embeds])
 
 with autocast("cuda"):
     for i, t in tqdm(enumerate(scheduler.timesteps), total=len(scheduler.timesteps)):
-        latents_inputs = torch.cat([latents]*2).to(torch_device)
-        latents_inputs = scheduler.scale_model_input(latents_inputs, t)
+        latent_input = torch.cat([latents]*2).to(torch_device)
+        latent_input = scheduler.scale_model_input(latent_input, t)
 
         with torch.no_grad():
-            noise_preds = unet.forward(latents_inputs, torch.tensor(t).to(torch_device), encoder_hidden_states=embeddings)
-        
-        uncond_noise, prompt_noise = noise_preds.sample.chunk(2)
+            noise_preds = unet.forward(latent_input, t, encoder_hidden_states=embeddings).sample
+
+        uncond_noise, prompt_noise = noise_preds.chunk(2)
         noise = uncond_noise + guidance_scale * (prompt_noise - uncond_noise)
 
-        latents = scheduler.step(noise, t, latents.to(torch_device)).prev_sample
+        latents = scheduler.step(noise, t, latents).prev_sample
 
-latents = 1 / 0.18215 * latents
-with torch.no_grad():
-    image = vae.decode(latents).sample
-
-# Display
-image = (image / 2 + 0.5).clamp(0, 1)
-image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
-images = (image * 255).round().astype("uint8")
-pil_images = [Image.fromarray(image) for image in images]
-pil_images[0]
+latents_to_pil(latents)[0]
